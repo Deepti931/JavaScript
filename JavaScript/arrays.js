9f229c2493e08fd2539bae51408ac53e09609391
Deepti931 <deeptishikha1999@gmail.com>
2022-02-14 17:57:37 +0530

    updation in arrays

--- JavaScript/arrays.js
+++ JavaScript/arrays.js
@@ -27,3 +27,37 @@ console.log("affter popping the elements:\n" +cars);
 cars.push("TATA");
 console.log("after pushing an element");
 console.log(cars);
+
+//3)unshift method-> this adds element at the starting of array
+cars.unshift("JLR");
+cars.unshift("Mushtang");
+console.log(cars);
+
+
+//4)shift method-> it removes element from 0th index of array
+cars.shift();
+cars.shift();
+console.log(cars); 
+console.log(cars.length);
+
+//2d array
+["a","b","c"]//array of strings
+[1,2,3]//array of numbers
+let array2d=[
+    ["BMW",2,null,45],
+    [4,true,6],
+    [7,"8",9],
+    [7,"8",9]
+];//array of arrays
+
+//contagious allocation of memory
+
+console.log(array2d);//prints 2d array
+console.table(array2d);//prints 2d array in the form of table
+let res=array2d[1];//[4,5,6]
+console.log("printing of res array");
+console.log(res);
+console.log("res of 2nd index");
+console.log(res[2]);
+console.log(array2d[1][2]);
+console.log(array2d[1][3]);//undefined
