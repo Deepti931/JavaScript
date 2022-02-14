77d159306155a81ac96185af894369efb7ad7a42
Deepti931 <deeptishikha1999@gmail.com>
2022-02-14 17:24:32 +0530

    second commit

--- JavaScript/function.js
+++ JavaScript/function.js
@@ -30,3 +30,31 @@ function calculator(str,a,b){
 let returnedFunc = calculator('add',2,3);
 console.log("returned function is\n" +returnedFunc);
 returnedFunc();
+
+//2) Function Expression
+//let variable name= function () {
+    //do something
+//}
+//name of variable is used to invoke the functions
+//variable_name();
+
+let sayHi = function abcd(){
+    console.log("hello guys i am function expression");
+}
+
+sayHi();
+//abcd();//error abcd is not defined
+
+//3)IIFE-> Imediately invoke function expression
+
+function add(a,b){
+    return a+b
+}
+add(2,3);
+
+let additionIIFE = (function add(a, b){
+    return a+b;
+})(20,30);
+
+console.log(additionIIFE);
+//console.log(additionIIFE(20,30));
